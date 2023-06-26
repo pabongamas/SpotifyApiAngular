c1ec575696923852b3d3a1be6120e878b1a63158
pabongamas <72658365+pabongamas@users.noreply.github.com>
2023-06-25 22:25:34 -0500

    se implementa vista para user

--- src/app/website/components/user/user.component.ts
+++ src/app/website/components/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { switchMap } from 'rxjs';
+import { AuthSpotifyService } from '../../../services/auth-spotify.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute,
+    private AuthSpotifyService:AuthSpotifyService
+  ) { }
+  userId:string|null=null;
+  userByIdData:any|null=[];
 
   ngOnInit(): void {
+    console.log(this.userByIdData);
+    this.route.paramMap
+    .pipe(
+      switchMap((params) => {
+        this.userId = params.get('id');
+        return this.AuthSpotifyService.getProfileUser(this.userId);
+      }),
+    // switchMap((requestTopTracks) => {
+    //   const parametros={
+    //     "limit":10,
+    //     "time_range":"short_term",
+      
+    // };
+    //   return this.AuthSpotifyService.getTopTracksByUser(parametros)
+    //   .pipe()
+    // })
+    )
+    .subscribe((data) => {
+      this.userByIdData = data;
+    });
+  }
+  goToProfile(url:string){
+    window.open(url);
   }
 
 }
