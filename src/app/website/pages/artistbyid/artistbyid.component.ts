f0ac79e3464528b06a636722102b41f43c54c446
pabongamas <72658365+pabongamas@users.noreply.github.com>
2023-07-15 12:47:38 -0500

    se implemantan botones en vista de artista para cargar ya se albums singles o recopilatorios , junto con sus efectos frontend css

--- src/app/website/pages/artistbyid/artistbyid.component.ts
+++ src/app/website/pages/artistbyid/artistbyid.component.ts
@@ -1,56 +1,76 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
-import { switchMap ,map,forkJoin,of } from 'rxjs';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, map, forkJoin, of } from 'rxjs';
 import { AuthSpotifyService } from '../../../services/auth-spotify.service';
 
 @Component({
   selector: 'app-artistbyid',
   templateUrl: './artistbyid.component.html',
-  styleUrls: ['./artistbyid.component.scss']
+  styleUrls: ['./artistbyid.component.scss'],
 })
 export class ArtistbyidComponent implements OnInit {
-
   constructor(
-    private route:ActivatedRoute,
-    private AuthSpotifyService:AuthSpotifyService
-  ) { }
-  artistId:string|null=null;
-  artistByIdData:any=[];
-  albumsByArtistData:any=[];
-  topTracksByArtistData:any=[];
-  totalElements:number=0;
+    private route: ActivatedRoute,
+    private AuthSpotifyService: AuthSpotifyService
+  ) {}
+  artistId: string | null = null;
+  artistByIdData: any = [];
+  albumsByArtistData: any = [];
+  topTracksByArtistData: any = [];
+  totalElements: number = 0;
+  activeButton: string = '';
   ngOnInit(): void {
     this.route.paramMap
-    .pipe(
-      switchMap((params) => {
-        this.artistId = params.get('id');
-        return this.AuthSpotifyService.getArtistsById(this.artistId);
-      }),
-      switchMap((requestAlbumsByArtist) => {
-        const parametros={
-          "include_groups":"album,single",
-          "offset":0,
-          "limit":7
-      };
-        return this.AuthSpotifyService.getAlbumsByArtistId(this.artistId,parametros).pipe(
-          switchMap((albums) => {
-            return forkJoin({
-              artist: of(requestAlbumsByArtist),
-              albums: of(albums),
-              topTracks: this.AuthSpotifyService.getTopTracksXArtist(this.artistId)
-            });
-          })
-        );
-      })
-     
-    )
-    .subscribe((data) => {
-      this.artistByIdData = data.artist;
-      this.albumsByArtistData=data.albums;
-      this.totalElements=data.albums.total;
-      this.topTracksByArtistData=data.topTracks.tracks;
-    });
-
+      .pipe(
+        switchMap((params) => {
+          this.artistId = params.get('id');
+          return this.AuthSpotifyService.getArtistsById(this.artistId);
+        }),
+        switchMap((requestAlbumsByArtist) => {
+          const parametros = {
+            include_groups: 'album',
+            offset: 0,
+            limit: 7,
+          };
+          return this.AuthSpotifyService.getAlbumsByArtistId(
+            this.artistId,
+            parametros
+          ).pipe(
+            switchMap((albums) => {
+              return forkJoin({
+                artist: of(requestAlbumsByArtist),
+                albums: of(albums),
+                topTracks: this.AuthSpotifyService.getTopTracksXArtist(
+                  this.artistId
+                ),
+              });
+            })
+          );
+        })
+      )
+      .subscribe((data) => {
+        console.log(data.albums);
+        this.activeButton='popular';
+        this.artistByIdData = data.artist;
+        this.albumsByArtistData = data.albums;
+        this.totalElements = data.albums.total;
+        this.topTracksByArtistData = data.topTracks.tracks;
+      });
   }
 
+  loadTypeWork(type: string) {
+    this.activeButton = type;
+    if(type==='popular'){
+      type='album';
+    }
+    const parametros = {
+      include_groups: type,
+      offset: 0,
+      limit: 7,
+    };
+    this.AuthSpotifyService.getAlbumsByArtistId(this.artistId, parametros)
+    .subscribe((data) => {
+      this.albumsByArtistData = data;
+    });
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,20 @@ module.exports = {
     extend: {
       colors:{
         'PrimaryColor':'#fff',
-        'ColorSpot':'#1ed760'
+        'ColorSpot':'#1ed760',
+        'activeButtonOption':'#fff',
+        'inactiveButtonOption':'#181818',
+        'hoverButtonOptionInactive':'#282828'
       },
       fontFamily: {
         'sans': ['Proxima Nova', ...defaultTheme.fontFamily.sans],
       },
       padding: {
         '15': '15px',
+      },
+      borderRadius:{
+        'roundedMainOption':'32px',
+        
       }
     },
   },
