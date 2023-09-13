5dfcf26dae469b55af0f9d48e042d94079e24bbb
pabongamas <pabongamas@gmail.com>
2023-09-13 18:10:42 -0500

    se revisa estilos para la info de los albums

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,13 +2,15 @@ import { CanActivateFn,ActivatedRouteSnapshot,  RouterStateSnapshot, UrlTree  } 
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import {TokenAuthServiceService} from '../services/token-auth-service.service';
+import { AuthSpotifyService } from '../services/auth-spotify.service';
 import { inject } from '@angular/core';
 export const authGuard: CanActivateFn = ( route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot) => {
 
     const isValidToken = inject(TokenAuthServiceService).isValidToken();
     if (!isValidToken) {
-      inject(Router).navigate(['/home']);
+      // inject(Router).navigate(['/home']);
+      inject(AuthSpotifyService).authorizeWithSpotify();
       return false;
     }
     return true;
